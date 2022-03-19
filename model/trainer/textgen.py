fe44df11f8d75b0d29fc195848d2e11928e33a1a
Michael Kuehl <hello@txs.vc>
2022-03-19 10:33:43 +0100

    upgraded to semantic versioning

--- model/trainer/textgen.py
+++ model/trainer/textgen.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 
-import os
-import sys
-import logging
 import argparse
-
+import logging
+import sys
+import os
 from .model import generate
 
 # https://stackoverflow.com/questions/35911252/disable-tensorflow-debugging-information
@@ -12,25 +11,30 @@ from .model import generate
 # 1 = INFO messages are not printed
 # 2 = INFO and WARNING messages are not printed
 # 3 = INFO, WARNING, and ERROR messages are not printed
-#os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
 def setup():
 
     parser = argparse.ArgumentParser()
 
+    # environment
     parser.add_argument(
-        '--job-dir',    # a working dir for training on the AI platform
-        default='job'
+        '--cache-dir',
+        default='cache'
     )
+
+    # job
     parser.add_argument(
         '--id',         # a unique id for the job
         default='id'
     )
     parser.add_argument(
-        '--cache-dir',
-        default='cache'
+        '--job-dir',    # a working dir for training on the AI platform
+        default='job'
     )
+
+    # generation
     parser.add_argument(
         '--prompt',
         required=True
